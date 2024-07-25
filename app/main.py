99a5bca3cf803c40eace8cb9c9ff54646ee4350b
Ivan <vanya.bidnenko@gmail.com>
2024-07-25 13:16:17 +0300

    Change to more descriptive variable names and avoid using ‘not’

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
         "errors": [format_linter_error(error) for error in errors],
-        "status": "passed" if not errors else "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
